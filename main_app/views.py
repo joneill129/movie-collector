906a1c8d76e4fb3e5c704158959754b1d6702c30
jpaige343 <jenniferoneill129@gmail.com>
2021-01-28 17:51:18 -0600

    render about template

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Hello! This is the movie collector home page!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>This is the about page!</h1>')
+    return render(request, 'about.html')
