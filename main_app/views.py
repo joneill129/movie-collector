dd538cdc6d911285b804e61f6c2de1fd754331a3
jpaige343 <jenniferoneill129@gmail.com>
2021-01-28 15:10:51 -0600

    set up and test home page route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Hello! This is the movie collector home page!</h1>')
