d61cda738f3874deb96c0c1f141876d7cdc4f7c4
jpaige343 <jenniferoneill129@gmail.com>
2021-01-28 19:21:20 -0600

    render movie index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('movies/', views.movies_index, name='index')
 ]
