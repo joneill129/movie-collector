0695a575edbad1ed72244c5d4557f9ec1d96b857
jpaige343 <jenniferoneill129@gmail.com>
2021-01-29 11:01:23 -0600

    set up Movie model.py and render Movies Index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    language = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Movie:
-    def __init__(self, title, year, genre, language, description):
-        self.title = title
-        self.year = year
-        self.genre = genre
-        self.language = language
-        self.description = description
-
-movies = [
-    Movie('The Dark Knight', 2008, 'Action/Adventure', 'English, Mandarin', 'SO GOOD'),
-    Movie('Pans Labyrinth', 2007, 'Drama/Fantasy', 'Spanish', 'In the Falangist Spain of 1944, the bookish young stepdaughter of a sadistic army officer escapes into an eerie but captivating fantasy world.'),
-]
+from .models import Movie
 
 # Create your views here.
 def home(request):
@@ -22,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def movies_index(request):
-    return render(request, 'movies/index.html', {'movies': movies})
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', { 'movies': movies })
