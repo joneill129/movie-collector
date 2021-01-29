229640f71c303f07b49eb722a3a8573bbc4d9a4e
jpaige343 <jenniferoneill129@gmail.com>
2021-01-29 11:54:24 -0600

    set up create route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('movies/', views.movies_index, name='index')
+    path('movies/', views.movies_index, name='index'),
+    path('movies/<int:movie_id>/', views.movies_detail, name='detail'),
+    path('movies/create', views.MovieCreate.as_view(), name='movies_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Movie
+import 
 
 # Create your views here.
 def home(request):
@@ -12,3 +14,12 @@ def about(request):
 def movies_index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', { 'movies': movies })
+
+def movies_detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/detail.html', { 'movie': movie})
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = '__all__'
+    
